51e397530c1f0c7023c45053219d916429ee9cdf
Lenz E <lenz73@gmail.com>
2013-05-09 19:44:19 +0200

    Environment set-up

---
+++ classes/ch/loway/oss/adb/VERSION.java
@@ -0,0 +1,15 @@
+
+package ch.loway.oss.adb;
+
+/**
+ *
+ *
+ * $Id$
+ * @author lenz
+ */
+public class VERSION {
+
+}
+
+// $Log$
+//
---
+++ libs/README.md
@@ -0,0 +1,2 @@
+Jars to be put here are created by the ''copyDeps'' task in build.gradle
+
